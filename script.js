19db05e5b5ab69190d9cc6fe0857e50f252890ad
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-01-07 00:50:29 -0600

    saving point

--- script.js
+++ script.js
@@ -2,32 +2,21 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
   
-
-
 // sets current date
 let currentDay = dayjs();
 $('#currentDay').text(currentDay.format('dddd MMMM D YY'));
 
 
-// sets current Time
-let currentTime = dayjs();
-$('#currentTime').text(currentTime.format('HH:mm '));
+// // sets current Time
+// let currentTime = dayjs();
+// $('#currentTime').text(currentTime.format('HH:mm '));
+
+let currentTime= dayjs().set('hour', 11).set('minute', 55).set('second', 15)
 
 
 // sets current hour -- may be used later if not delete
-let currentHour = dayjs().format('HH');
+// let currentHour = dayjs().format('HH');
 
 
 
@@ -75,45 +64,39 @@ const timeSlots = {
 const textArea = document.getElementsByClassName('description');
 
 
-
 // looping through the timeSlots object and identifying how the value compares to the current time and adding the correct class.
-// need to go in and get "present time" class working. currently its only displaying past and future
-for (let key in timeSlots){
-  if(timeSlots.hasOwnProperty(key)){
+for (let key in timeSlots) {
+  if (timeSlots.hasOwnProperty(key)) {
     const $element = $(`#${timeSlots[key].id}`);
 
-    if (currentTime.isBefore(timeSlots[key].value)){
+    if (currentTime.isBefore(timeSlots[key].value)) {
       $element.addClass('future');
-    }
-    else if (currentTime.isAfter(timeSlots[key].value)){
-      $element.addClass('past')
-    }
-    else if (currentTime.isSame(timeSlots[key].value))
-    {
-      $element.addClass('present')
+    } else if (currentTime.isAfter(timeSlots[key].value)) {
+      $element.addClass('past');
+    } else {
+      $element.addClass('present');
     }
   }
 }
 
-  
 // An event listener that saves input to local storage when any save button on the page is clicked
-
 const saveBtn = document.querySelectorAll('.btn');
 
-saveBtn.forEach(function(saveBtn){
- saveBtn.addEventListener('click', function(){
-   for(let i=0; i < textArea.length; i++){
-     localStorage.setItem('user input' + i, textArea[i].value);
-   }
-
-   window.addEventListener('load', function () {
+saveBtn.forEach(function (saveBtn) {
+  saveBtn.addEventListener('click', function () {
     for (let i = 0; i < textArea.length; i++) {
-      const savedValue = localStorage.getItem('user input' + i);
-      textArea[i].value = savedValue;
+      localStorage.setItem('user input' + i, textArea[i].value);
     }
   });
-})
- })
+});
+
+// Event listener to load saved values from local storage when the page is loaded
+window.addEventListener('load', function () {
+  for (let i = 0; i < textArea.length; i++) {
+    const savedValue = localStorage.getItem('user input' + i);
+    textArea[i].value = savedValue;
+  }
+});
 
 
 
